6b07a4985074507e3d68108267fc94ff2f694490
Josafat Daniel Salcedo Sambrano <danielox@live.com>
2015-10-16 21:51:11 -0500

    Se agregan archivos para definir roles

---
+++ app/Models/CatalogoRol.php
@@ -0,0 +1,40 @@
+<?php namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class CatalogoRol extends Model{
+
+    protected $table = 'catalogo_rol';
+    protected $fillable = [
+        'clave',
+        'titulo',
+        'descripcion'
+    ];
+
+    /**
+     * Crea o actualiza un registro
+     */
+    static function createOrUpdate($data, $keys) {
+        $registro = self::where($keys)->first();
+        if (is_null($registro)) {
+            $registro = self::create($data);
+        } else {
+            self::where($keys)->update($data);
+            $registro = self::where($keys)->first();
+        }
+
+        return $registro;
+    }
+
+    /**
+     * one-to-many relationship method
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function usuario(){
+        return $this->belongsToMany('App\Models\Usuario', 'rol_usuario', 'rol_id', 'usuario_id')->withTimestamps();
+    }
+
+
+}
---
+++ app/Models/RolUsuario.php
@@ -0,0 +1,29 @@
+<?php namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class RolUsuario extends Model{
+
+    protected $table = 'rol_usuario';
+    protected $fillable = [
+        'usuario_id',
+        'rol_id'
+    ];
+
+    /**
+     * Crea o actualiza un registro
+     */
+    static function createOrUpdate($data, $keys) {
+        $registro = self::where($keys)->first();
+        if (is_null($registro)) {
+            $registro = self::create($data);
+        } else {
+            self::where($keys)->update($data);
+            $registro = self::where($keys)->first();
+        }
+
+        return $registro;
+    }
+
+}
