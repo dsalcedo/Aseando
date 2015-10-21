1b9f4f772b6fa93a9f4081a58325a9768314282e
danieluyo <dsalcedo@outlook.com>
2015-10-21 01:03:14 -0500

    Se da estilo de codificacion de acuerdo a PSR-2 y PSR-4

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,18 +11,55 @@
 |
 */
 
-//Website
-Route::get('/', [ 'uses'=>'Website@index', 'as'=>'website.index' ]);
-Route::get('acceso', [ 'uses'=>'Website@acceso', 'as'=>'website.acceso' ]);
-Route::get('auth/login', [ 'uses'=>'Website@authLogin', 'as'=>'website.auth.login' ]);
-Route::post('auth/login', [ 'uses'=>'Auth\AuthController@postLogin', 'as'=>'website.post-acceso' ]);
-Route::get('reclutamiento', [ 'uses'=>'Website@acceso', 'as'=>'website.reclutamiento' ]);
+    /**
+     * Rutas del sitio web principal
+     */
+    Route::get('/', [
+        'uses' => 'Website@index',
+        'as'   => 'website.index'
+    ]);
+    Route::get('como-funciona', [
+        'uses' => 'Website@comoFunciona',
+        'as'   => 'website.como-funciona'
+    ]);
+    Route::get('precios', [
+        'uses' => 'Website@precios',
+        'as'   => 'website.precios'
+    ]);
+    Route::get('preguntas-frecuentes', [
+        'uses' => 'Website@preguntas',
+        'as'   => 'website.preguntas-frecuentes'
+    ]);
+    Route::get('acceso', [
+        'uses' => 'Website@acceso',
+        'as'   => 'website.acceso'
+    ]);
+    Route::get('reclutamiento', [
+        'uses' => 'Website@acceso',
+        'as'   => 'website.reclutamiento'
+    ]);
+    Route::get('auth/login', [
+        'uses' => 'Website@authLogin',
+        'as'   => 'website.auth.login'
+    ]);
+    Route::post('auth/login', [
+        'uses' => 'Auth\AuthController@postLogin',
+        'as'   => 'website.post-acceso'
+    ]);
 
 
-//Cuenta
-Route::group(['prefix' => 'cuenta', 'middleware' => 'auth'], function () {
-    Route::get('/', [ 'uses'=>'Cuenta@index', 'as'=>'cuenta.index' ]);
-});
+    //Cuenta
+    Route::group([
+        'prefix'     => 'cuenta',
+        'middleware' => 'auth'
+    ], function () {
+
+        Route::get('/', [
+            'uses' => 'Cuenta@index',
+            'as'   => 'cuenta.index'
+        ]);
+
+    });
 
 
 
